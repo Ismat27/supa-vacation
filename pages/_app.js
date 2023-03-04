38c6a0fd48c65e95909705d1da377050c633137c
ismat27 <adegbiteismail27@gmail.com>
2023-03-04 19:50:17 +0100

    wrap app with next session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </SessionProvider>
   );
 }
 
