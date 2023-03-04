6ab1cf7d97c1479d4e8ed0286234dea798628aea
ismat27 <adegbiteismail27@gmail.com>
2023-03-04 22:24:54 +0100

    update image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['rvmslziajycwmkcdvfpc.supabase.co', 'rvmslziajycwmkcdvfpc.supabase.in']
+    domains: [
+      'rvmslziajycwmkcdvfpc.supabase.co',
+      'rvmslziajycwmkcdvfpc.supabase.in',
+      'lh3.googleusercontent.com'
+    ]
   }
 }
