faa6fd5142796c6cc541c39c1dda9c0b53028460
ismat27 <adegbiteismail27@gmail.com>
2023-03-04 12:54:22 +0100

    load data from server

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+export async function getServerSideProps () {
+  const homes = await prisma.home.findMany()
+  return {
+    props: {homes: JSON.parse(JSON.stringify(homes))}
+  }
+}
 
 import homes from 'data.json';
 
-export default function Home() {
+export default function Home({homes}) {
   return (
     <Layout>
       <h1 className="text-xl font-medium text-gray-800">
