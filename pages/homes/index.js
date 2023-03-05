f52c5aa44a1aa11ba41b29b626ffb0b579a7e66b
ismat27 <adegbiteismail27@gmail.com>
2023-03-05 10:49:26 +0100

    page to show logged in user listed homes

---
+++ pages/homes/index.js
@@ -0,0 +1,45 @@
+import Grid from "@/components/Grid";
+import Layout from "@/components/Layout";
+import { PrismaClient } from "@prisma/client";
+import { getSession } from "next-auth/react";
+
+const prisma = new PrismaClient()
+
+export const getServerSideProps = async (context) => {
+    const session = await getSession(context)
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
+    const homes = await prisma.home.findMany({
+        where: {owner: {email: session.user.email}},
+        orderBy: {updatedAt: 'asc'}
+    })
+
+    return {
+        props: {
+            homes: JSON.parse(JSON.stringify(homes))
+        }
+    }
+}
+
+const UserListedHomes = ({homes}) => {
+    return (
+        <Layout>
+            <h1 className="text-xl font-medium text-gray-800">Your listed homes</h1>
+            <p className="text-gray-500">
+                Manage your homes and update your listings
+            </p>
+            <div className="mt-8">
+                <Grid homes={homes}/>
+            </div>
+        </Layout>
+    )
+}
+
+export default UserListedHomes
