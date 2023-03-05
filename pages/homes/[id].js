bc3eeb5111601e5201e660b2614852972a14b5d0
ismat27 <adegbiteismail27@gmail.com>
2023-03-05 13:54:31 +0100

    enabled the owner of a home to be able to edit and delete the home

--- pages/homes/[id].js
+++ pages/homes/[id].js
@@ -1,34 +1,76 @@
 // pages/homes/[id].js
 import { PrismaClient } from '@prisma/client';
+import Image from 'next/image';
+import Layout from '@/components/Layout';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import toast from 'react-hot-toast';
 const prisma = new PrismaClient()
 
 export async function getStaticPaths () {
     const homes = await prisma.home.findMany({
-        select: {id: true}
+      select: {id: true}
     })
     const paths = homes.map(home => ({
-        params: {id: home.id}
+      params: {id: home.id}
     }))
     return {
-        paths: paths,
-        fallback: false
+      paths: paths,
+      fallback: false
     }
 }
 
 export async function getStaticProps ({params}) {
     const {id} = params
     const home = await prisma.home.findUnique({
-        where: {id: id}
+      where: {id: id}
     })
     return {
-        props: JSON.parse(JSON.stringify(home))
+      props: JSON.parse(JSON.stringify(home))
     }
 }
 
-import Image from 'next/image';
-import Layout from '@/components/Layout';
-
 const ListedHome = (home = null) => {
+  const router = useRouter()
+  const [isOwner, setIsOwner] = useState(false)
+  const [deleting, setDeleting] = useState(false)
+  const {data:session} = useSession()
+
+  const deleteHome = async () => {
+    let toastId;
+    try {
+      toastId = toast.loading('Deleting...');
+      setDeleting(true);
+      // Delete home from DB
+      await axios.delete(`/api/homes/${home.id}`);
+      // Redirect user
+      toast.success('Successfully deleted', { id: toastId });
+      router.push('/homes');
+    } catch (e) {
+      console.log(e);
+      toast.error('Unable to delete home', { id: toastId });
+      setDeleting(false);
+    }
+  };
+
+  useEffect(() => {
+    async function  getOwner() {
+      if (!home?.id || !session?.user) {
+        setIsOwner(false)
+        return
+      }
+      try {
+        const {data:owner}= await axios.get(`/api/homes/${home.id}/owner`)
+        setIsOwner(owner.id === home.ownerId)
+      } catch (error) {
+        setIsOwner(false)
+      }
+    }
+    getOwner()
+  }, [session?.user])
+
   return (
     <Layout>
       <div className="max-w-screen-lg mx-auto">
@@ -51,6 +93,29 @@ const ListedHome = (home = null) => {
               </li>
             </ol>
           </div>
+
+          {isOwner ? (
+            <div className="flex items-center space-x-2">
+              <button
+                type="button"
+                disabled={deleting}
+                onClick={() => router.push(`/homes/${home.id}/edit`)}
+                className="px-4 py-1 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition rounded-md disabled:text-gray-800 disabled:bg-transparent disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                Edit
+              </button>
+
+              <button
+                type="button"
+                disabled={deleting}
+                onClick={() => deleteHome()}
+                className="rounded-md border border-rose-500 text-rose-500 hover:bg-rose-500 hover:text-white focus:outline-none transition disabled:bg-rose-500 disabled:text-white disabled:opacity-50 disabled:cursor-not-allowed px-4 py-1"
+              >
+                {deleting? 'Deleting...': "Delete"}
+              </button>
+            </div>
+          ) : null}
+
         </div>
 
         <div className="mt-6 relative aspect-w-16 aspect-h-9 bg-gray-200 rounded-lg shadow-md overflow-hidden">
