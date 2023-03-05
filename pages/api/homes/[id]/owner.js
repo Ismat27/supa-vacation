7af133324e4a3e9dc0c3c9f3f7aba63ea53063e7
ismat27 <adegbiteismail27@gmail.com>
2023-03-05 13:55:29 +0100

    api route to get the owner of a home

---
+++ pages/api/homes/[id]/owner.js
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+import { getSession } from "next-auth/react";
+
+const prisma = new PrismaClient()
+
+const handler = async (req, res) => {
+    if (req.method !== 'GET') {
+        res.setHeader('Allow', ['GET']);
+        res.status(405).json({
+            message: 'not allowed'
+        })
+    }
+    const session = await getSession({req})
+    if (!session) {
+        res.status(401).json({
+            message: 'unauthorized request'
+        })
+    }
+    try {
+        const { id } = req.query
+        const home = await prisma.home.findUnique({
+            where: {id: id},
+            select: {owner: true}
+        })
+        if (!home) {
+            res.status(404).json({
+                message: 'incorrect home credientials'
+            })
+        }
+        res.status(200).json(home.owner)
+    } catch (error) {
+        res.status(422).json({
+            message: 'unable to handle request'
+        })
+    }
+}
+
+export default handler
